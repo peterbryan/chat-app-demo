6191e2bca84aa4c39353eadbef19bbd1e3c1ca94
Peter Bryan <peterbryanr@gmail.com>
2017-06-30 19:47:08 -0700

    small fix

--- server/server.js
+++ server/server.js
@@ -2,13 +2,13 @@ const path= require('path');
 const express= require('express');
 
 const PublicPath= path.join(__dirname,'../public');
-const port= process.env.PORT ||3000;
+const port= process.env.PORT || 3000;
 const app =express();
 
 
 app.use(express.static(PublicPath));
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
 
 	console.log(`Server is up on ${port}`);
 })
