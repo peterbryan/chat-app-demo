44d8c000cc5f3f208b6d9f138e59025359824460
Peter Bryan <peterbryanr@gmail.com>
2017-07-01 17:55:11 -0700

    Added chat connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,26 @@
 const path= require('path');
+const http= require('http');
 const express= require('express');
+const socketIO=require('socket.io');
 
 const PublicPath= path.join(__dirname,'../public');
 const port= process.env.PORT || 3000;
 const app =express();
-
+const server = http.createServer(app);
+const io=socketIO(server);
 
 app.use(express.static(PublicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+console.log('New user connected');
+
+socket.on('disconnect',()=>{
+	console.log('Client disconnected');
+});
+
+});
+
+server.listen(port,()=>{
 
 	console.log(`Server is up on ${port}`);
 })
