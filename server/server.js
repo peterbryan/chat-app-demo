ef5338f92d9376a855c84f36bd94744f9ca54cf8
Peter Bryan <peterbryanr@gmail.com>
2017-07-15 02:50:38 -0700

    Added geolocation, test case

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http= require('http');
 const express= require('express');
 const socketIO=require('socket.io');
 
-const {generateMessage}= require('./utils/message');
+const {generateMessage, generateLocationMessage}= require('./utils/message');
 
 const PublicPath= path.join(__dirname,'../public');
 const port= process.env.PORT || 3000;
@@ -24,12 +24,13 @@ io.on('connection',(socket)=>{
 		console.log('createEmail',message);
 		io.emit('newMessage',generateMessage(message.from,message.text));
 		callback('This is from the server');
-	// socket.broadcast.emit('newMessage',{
-	// 		from: message.from,
-	// 		text:message.text,
-	// 		createdAt: new Date.getTime();
-	// 	})
 	});
+
+	socket.on('createLocationMessage',(coords)=> {
+		io.emit('newLocationMessage',generateLocationMessage('Admin',coords.latitude,coords.longitude));
+	});
+
+
 
 
 	socket.on('disconnect',()=>{
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,7 +1,7 @@
 var expect =require('expect');
 
 
-var {generateMessage} = require('./message')
+var {generateMessage, generateLocationMessage} = require('./message')
 describe('generateMessage', ()=>{
 	it('should generate correct message object',()=>{
 		var from ='Jen';
@@ -16,3 +16,15 @@ describe('generateMessage', ()=>{
 	});
 
 });
+
+describe('generateLocationMessage',()=>{
+	it('should generate correct location object',()=>{
+		var from ='Someone';
+		let latitude=10;
+		let longitude=75;
+		var url='https://www.google.com/maps?q=10,75';
+		var locationMessage=generateLocationMessage(from,latitude,longitude);
+		expect(locationMessage.createdAt).toBeA('number');
+		expect(locationMessage).toInclude({from,url})
+	})
+})
