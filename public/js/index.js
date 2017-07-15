70a4b12b7ab75cca2a2b73fddc679bcc03aab6a1
Peter Bryan <peterbryanr@gmail.com>
2017-07-15 02:54:27 -0700

    Added index.html button update

--- public/js/index.js
+++ public/js/index.js
@@ -28,3 +28,28 @@
 				//ack function
 			});
 		});
+
+		socket.on('newLocationMessage',function(message){
+			var li =jQuery('<li></li>');
+			var a = jQuery('<a target="_blank">My current location</a>');
+			li.text(`${message.from}: `);
+			a.attr('href',message.url)
+			li.append(a);
+			jQuery('#messages').append(li);
+
+		})
+
+		const locationButton = jQuery('#send-location');
+		locationButton.on('click',function(){
+			if(!navigator.geolocation){
+				return alert('geolocation not supported by your browser');
+			}
+		navigator.geolocation.getCurrentPosition(function(position){
+			socket.emit('createLocationMessage',{
+				latitude: position.coords.latitude,
+				longitude: position.coords.longitude
+			});
+		}, function(){
+			alert('unable to fetch location');
+			});
+		});
