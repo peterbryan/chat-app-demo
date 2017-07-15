05a237b0a744cb0d1009e5378b63ff7cce6403ea
Peter Bryan <peterbryanr@gmail.com>
2017-07-15 03:46:40 -0700

    Added css

--- public/js/index.js
+++ public/js/index.js
@@ -18,14 +18,16 @@
 			jQuery('#messages').append(li);
 		});
 
+
+		var messageTextbox=jQuery('[name=message]')
 		jQuery('#message-form').on('submit',function(e){
 			e.preventDefault();
 
 			socket.emit('createMessage',{
 				from:'User',
-				text: jQuery('[name=message]').val()
+				text: messageTextbox.val()
 			}, function(){
-				//ack function
+				messageTextbox.val('')
 			});
 		});
 
@@ -44,12 +46,16 @@
 			if(!navigator.geolocation){
 				return alert('geolocation not supported by your browser');
 			}
+			locationButton.attr('disabled', 'disabled').text('Sending location...');
+
 		navigator.geolocation.getCurrentPosition(function(position){
+			locationButton.removeAttr('disabled').text('Send location');
 			socket.emit('createLocationMessage',{
 				latitude: position.coords.latitude,
 				longitude: position.coords.longitude
 			});
 		}, function(){
+			locationButton.removeAttr('disabled').text('Send location');
 			alert('unable to fetch location');
 			});
 		});
