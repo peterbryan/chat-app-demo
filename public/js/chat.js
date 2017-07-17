a3fd1bcf0eced1aeaba9f75137b183a898af5811
Peter Bryan <peterbryanr@gmail.com>
2017-07-16 18:35:48 -0700

    Added landing page

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,23 @@
 		var socket=io();
 
+		function scrollToBottom(){
+			//Selectors
+			let messages=jQuery('#messages');
+			let newMessage=messages.children('li:last-child')
+			//Heights
+			let clientHeight=messages.prop('clientHeight');
+			let scrollTop= messages.prop('scrollTop');
+			let scrollHeight=messages.prop('scrollHeight');
+			let newMessageHeight=newMessage.innerHeight();
+			let lastMessageHeight=newMessage.prev().innerHeight();
+
+			if(clientHeight +scrollTop +newMessageHeight +lastMessageHeight >= scrollHeight){
+				messages.scrollTop(scrollHeight);
+			}
+
+
+		}
+
 		socket.on('connect',function(){
 			console.log('Connected to server');
 
@@ -12,10 +30,14 @@
 
 		socket.on('newMessage',function(message){
 			let formattedTime = moment(message.createdAt).format('h:mm a');
-			const li=jQuery('<li></li>');
-			li.text(`${message.from} ${formattedTime}: ${message.text} `);
-
-			jQuery('#messages').append(li);
+			let template=jQuery('#message-template').html();
+			let html =Mustache.render(template,{
+				text: message.text,
+				from: message.from,
+				createdAt: formattedTime
+			});
+			jQuery('#messages').append(html);
+			scrollToBottom();
 		});
 
 
@@ -33,12 +55,14 @@
 
 		socket.on('newLocationMessage',function(message){
 			let formattedTime =moment(message.createdAt).format('h:mm a');
-			var li =jQuery('<li></li>');
-			var a = jQuery('<a target="_blank">My current location</a>');
-			li.text(`${message.from} ${formattedTime}: `);
-			a.attr('href',message.url)
-			li.append(a);
-			jQuery('#messages').append(li);
+			let template=jQuery('#locationMessage-template').html();
+			let html=Mustache.render(template,{
+				from:message.from,
+				createdAt:formattedTime,
+				url:message.url
+			});
+			jQuery('#messages').append(html)
+			scrollToBottom();
 
 		})
 
